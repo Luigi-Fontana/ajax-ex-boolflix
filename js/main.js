a971d4fcae5a2cc59c5c19247b76700b38d81346
Luigi Fontana <luigifontana92@gmail.com>
2020-03-18 13:13:31 +0100

    Inizio Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var baseUrl = 'https://api.themoviedb.org/3';
-    $.ajax({
-        url: baseUrl + '/search/movie',
-        data: {
-            api_key: '826bc99f9afd0a331c43a3695d0d0263',
-            query: 'Lo Hobbit',
-            language: 'it-IT',
-            page: '1',
-            include_adult: 'false'
-        },
-        method: 'GET',
-        success: function (data) {
-            var films = data.results;
-            for (var i = 0; i < films.length; i++) {
-                var film = films[i];
-                console.log(film);
+
+    var source = $('#card-template').html();
+    var template = Handlebars.compile(source);
+
+    $('#btn-search').click(function () {
+        var input = $('#search').val();
+        console.log(input);
+        $('#search').val('');
+        $.ajax({
+            url: baseUrl + '/search/movie',
+            data: {
+                api_key: '826bc99f9afd0a331c43a3695d0d0263',
+                query: input,
+                language: 'it-IT'
+            },
+            method: 'GET',
+            success: function (data) {
+                var films = data.results;
+                for (var i = 0; i < films.length; i++) {
+                    var film = films[i];
+                    console.log(film);
+                    var filmTemplate = {
+                        titolo: film.title,
+                        titoloOriginale: film.original_title,
+                        lingua: film.original_language,
+                        voto: film.vote_average
+                    };
+                        var cardFilm = template(filmTemplate);
+                        $('.container-card').append(cardFilm);
+                };
+            },
+            error: function () {
+                alert('C\'è qualquadra che non cosa');
             }
-        },
-        error: function () {
-            alert('C\'è qualquadra che non cosa');
-        }
+        });
     });
 
 });
