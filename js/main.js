2fc5d6f3834b761684f63fa60c93928d15536f1a
Luigi Fontana <luigifontana92@gmail.com>
2020-03-23 12:21:51 +0100

    Miglioramento codice JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // Definizioni Variabili usate in seguito
+    var apiBaseUrl = 'https://api.themoviedb.org/3';
+    var coverBaseUrl = 'https://image.tmdb.org/t/p/';
+    var coverBaseSize = 'w342';
 
     var cardSource = $('#card-template').html(); // Clono il template di Handlebars e glielo dò in pasto
     var cardTemplate = Handlebars.compile(cardSource);
@@ -12,101 +16,118 @@ $(document).ready(function () {
         };
     });
 
-    function search() { // Funzione ricerca con autoeliminazione input e controllo sull'inserimento di almeno un carattere per la ricerca
+    function search() { // Funzione di ricerca con autoeliminazione input e controllo sull'inserimento di almeno un carattere per la ricerca
         var input = $('#search').val();
         $('#search').val('');
         if (input.length > 0) {
             $('.card').remove(); // Innanzittuo rimuovo tutte le card prima di crearne di nuove
-            apiCall(input);
+            apiCall('movie', input);
+            apiCall('tv', input);
         } else {
             alert('Campo di ricerca vuoto');
         };
     };
 
-    function apiCall(query) { // Funzione di chiamata API con query in entrata
-        var baseUrl = 'https://api.themoviedb.org/3';
+    function apiCall(type, query) { // Funzione di chiamata API con tipo e query in entrata
         $.ajax({
-            url: baseUrl + '/search/movie',
+            url: apiBaseUrl + '/search/' + type,
             data: {
-                api_key: '826bc99f9afd0a331c43a3695d0d0263',
+                api_key: 'e99307154c6dfb0b4750f6603256716d',
                 query: query,
                 language: 'it-IT'
             },
             method: 'GET',
             success: function (data) {
                 var cards = data.results;
-                printMovie(cards);
+                printCard(type, cards);
+
             },
-            error: function () {
-                alert('Database Error');
-            }
-        });
-        $.ajax({
-            url: baseUrl + '/search/tv',
-            data: {
-                api_key: '826bc99f9afd0a331c43a3695d0d0263',
-                query: query,
-                language: 'it-IT'
-            },
-            method: 'GET',
-            success: function (data) {
-                var cards = data.results;
-                printTv(cards);
-            },
-            error: function () {
+            error: function (err) {
                 alert('Database Error');
             }
         });
     };
 
-    function printMovie(array) { // Funzione di stampa della Card con Handlebars con array di oggetti in entrata
-        var baseImgUrl = 'https://image.tmdb.org/t/p/';
+    function printCard(type, array) { // Funzione di stampa della Card con Handlebars con tipo e array di oggetti in entrata
         for (var i = 0; i < array.length; i++) {
             var card = array[i];
+            if (type == 'movie') {
+                titolo = card.title;
+                titoloOriginale = card.original_title;
+            } else if (type == 'tv') {
+                titolo = card.name;
+                titoloOriginale = card.original_name;
+            }
             var cardInfos = {
-                cover: baseImgUrl + '/w185/' + card.poster_path,
-                titolo: card.title,
-                titoloOriginale: card.original_title,
-                lingua: card.original_language,
-                voto: transformVote(card.vote_average)
+                cover: linkCover(card.poster_path),
+                titolo: titolo,
+                titoloOriginale: titoloOriginale,
+                lingua: createFlag(card.original_language),
+                voto: transformVote(card.vote_average),
+                stars: createStars(transformVote(card.vote_average))
             };
             var cardHtml = cardTemplate(cardInfos);
             $('.container').append(cardHtml);
             deleteTitle(cardInfos.titolo, cardInfos.titoloOriginale);
-            stars(cardInfos.voto);
         };
     };
 
-    function printTv(array) { // Funzione di stampa della Card con Handlebars con array di oggetti in entrata
-        var baseImgUrl = 'https://image.tmdb.org/t/p/';
-        for (var i = 0; i < array.length; i++) {
-            var card = array[i];
-            var cardInfos = {
-                cover: baseImgUrl + '/w185/' + card.poster_path,
-                titolo: card.name,
-                titoloOriginale: card.original_name,
-                lingua: card.original_language,
-                voto: transformVote(card.vote_average)
-            };
-            var cardHtml = cardTemplate(cardInfos);
-            $('.container').append(cardHtml);
-            deleteTitle(cardInfos.titolo, cardInfos.titoloOriginale);
-            stars(cardInfos.voto);
-        };
-    };
+    function createFlag(language) { // Funzione di sostituzione della lingua in bandiere della nazione corrispondente
+        var flags = [
+            'be',
+            'cn',
+            'cs',
+            'cz',
+            'da',
+            'de',
+            'en',
+            'es',
+            'fr',
+            'it',
+            'ja',
+            'nl',
+            'pl',
+            'pt',
+            'ro',
+            'ru',
+            'ta',
+            'tl',
+            'tr'
+        ];
+        if (flags.includes(language)) {
+            var flag = '<img class="flag" src="img/flags/' + language + '.png" alt="' + language + '">';
+            return flag;
+        }
+        return language;
+    }
 
-    function transformVote(vote) { // Funzione che converte il voto in decimi in quinti
+    function transformVote(vote) { // Funzione di conversione del voto da decimi a quinti
         return Math.ceil(vote / 2);
     };
 
-    function stars(vote) { // Funzione che calcola la percentuale di larghezza che deve avere il div delle stelle piene
-        var percentage = (vote * 100) / 5;
-        $(".card:last-child").find(".fill-stars").css( "width", percentage + "%");
+    function createStars(vote) { // Funzione di creazione stelle in numero uguale al voto in quinti
+        var stars = '';
+        for (var i = 1; i <= 5; i++) {
+            if (i <= vote) {
+                stars += '<i class="fas fa-star"></i>';
+            } else {
+                stars += '<i class="far fa-star"></i>';
+            }
+        }
+        return stars;
     };
 
-    function deleteTitle(title, original) { // Funzione che elimina il titolo originale se è uguale al titolo
+    function deleteTitle(title, original) { // Funzione di eliminazione del titolo originale se è uguale al titolo
         if (title == original) {
             $(".card:last-child").find(".title").remove();
         }
     };
+
+    function linkCover(link) { // Funzione di creazione del link alle cover e immagine standard in caso di cover assente
+        if (link !== null) {
+            return coverBaseUrl + coverBaseSize + link;
+        } else {
+            return 'img/nocover.jpeg';
+        }
+    }
 });
